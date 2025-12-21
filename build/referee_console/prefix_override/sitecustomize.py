78138d064a22f9427bd2dffdf5fc64be9fd3cf03
ybasova <y.basova@g.nsu.ru>
2025-12-21 17:52:19 +0700

    deleted build, log, install

--- build/referee_console/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/natalia/Documents/STUDING/ROS_bit_pel/install/referee_console'
--- build/robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/natalia/Documents/STUDING/ROS_bit_pel/my_robot/robot_description
-# Build directory: /home/natalia/Documents/STUDING/ROS_bit_pel/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
